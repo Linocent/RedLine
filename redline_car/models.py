082b3ce8d6145cd321db2856e459702bc02e18ee
le_shtroumpf <timotheelinocent@gmail.com>
2022-08-19 23:34:56 +0200

    update model with "poids".

--- redline_car/models.py
+++ redline_car/models.py
@@ -2,18 +2,20 @@ from django.db import models
 
 
 class Categorie(models.Model):
+    id = models.AutoField(primary_key=True)
     nom = models.CharField(max_length=200)
     poids = models.IntegerField()
 
     def __str__(self):
-        return f"{self.nom}"
+        return f"{self.nom, self.poids, self.id}"
 
 
 class Vehicule(models.Model):
+    id = models.AutoField(primary_key=True)
     nom = models.CharField(max_length=100)
     categorie = models.ForeignKey(Categorie, on_delete=models.CASCADE)
     prix = models.IntegerField(default=0)
     thumbnail = models.FilePathField()
 
     def __str__(self):
-        return f"{self.nom, self.prix}"
+        return f"{self.nom, self.prix, self.categorie, self.thumbnail, self.id}"
