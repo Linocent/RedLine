8af025954ba32e71d61c55b0e0fd02e80ed461ed
le_shtroumpf <timotheelinocent@gmail.com>
2022-08-19 23:35:24 +0200

    Add the search and selection fonctionnality

--- redline_car/urls.py
+++ redline_car/urls.py
@@ -4,5 +4,10 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'admin/', admin.site.urls),
-    url('^&', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
+    url(
+        r'^details/(?P<vehicule_id>[0-9]+)/$',
+        views.detail,
+        name='details',
+    )
 ]
--- redline_car/views.py
+++ redline_car/views.py
@@ -1,5 +1,40 @@
 from django.shortcuts import render
+from .models import Categorie, Vehicule
 
 
 def index(request):
+    query = request.GET.get('query')
+    if query:
+        answer = Vehicule.objects.filter(nom__icontains=query)
+        print(answer)
+        if answer.exists():
+            if len(answer) > 1:
+                return render(
+                    request,
+                    'redline_car/search.html',
+                    {'answer': answer, 'query': query}
+                )
+            else:
+                detail(request, answer.id)
+        else:
+            message = f"Aucun résultat trouvé pour {query}."
+            return page_not_found(request, message)
     return render(request, "redline_car/index.html")
+
+
+def detail(request, vehicule_id):
+    vehicule = Vehicule.objects.filter(id__exact=vehicule_id)
+    print(vehicule)
+    return render(
+        request,
+        'redline_car/detail.html',
+        context={'vehicule': vehicule},
+    )
+
+
+def page_not_found(request, message):
+    return render(
+        request,
+        'redline_car/404.html',
+        {'message': message}
+    )
