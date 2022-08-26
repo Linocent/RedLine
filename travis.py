59d8d55e9eef18c7a9d4bb5f1cd1765426f91d32
le_shtroumpf <timotheelinocent@gmail.com>
2022-08-26 18:47:41 +0200

    Add travis

---
+++ travis.py
@@ -0,0 +1,13 @@
+import os
+from concession import *
+
+DATABASE = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'redline',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    },
+}
