94f6cb61587daf3c677ce82512a3060bdadcbb58
le_shtroumpf <timotheelinocent@gmail.com>
2022-09-11 11:14:19 +0200

    Add unit test for cmd

--- redline_car/tests/test_cmd.py
+++ redline_car/tests/test_cmd.py
@@ -7,8 +7,47 @@ from redline_car.models import (
 
 
 class CategoryTest(TestCase):
-    def test_cmd(self):
+    def setUp(self):
+        self.categorie = Categorie.objects.create(
+            id=1,
+            nom='cat_to_remove',
+            poids='10',
+        )
+        self.categorie.save()
+
+    def test_remove_old_entry(self):
+        call_command('category')
+        self.assertFalse(
+            Categorie.objects.filter(
+                nom__icontains='cat_to_remove'
+            ))
+
+    def test_category_cmd(self):
         call_command('category')
         self.assertEqual(Categorie.objects.count(), 16)
+
+
+class VehiculeTest(TestCase):
+    def setUp(self):
+        call_command('category')
+
+        self.vehicule = Vehicule.objects.create(
+            id=1,
+            nom='vehicule_to_remove',
+            categorie=Categorie.objects.get(nom__icontains='Moto'),
+            prix='10000',
+            thumbnail='img/thumbnail.jpg'
+        )
+        self.vehicule.save()
+
+    def test_remove_old_entry(self):
+        call_command('vehicule')
+        self.assertFalse(
+            Vehicule.objects.filter(
+                nom__icontains='vehicule_to_remove'
+            ))
+
+    def test_vehicule_cmd(self):
         call_command('vehicule')
         self.assertEqual(Vehicule.objects.count(), 380)
+
